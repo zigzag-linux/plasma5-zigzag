4a1b57dfce7f19d91da8c467881ef319b095173f
Maciek Krawiec <142805+mkrawiec@users.noreply.github.com>
2022-02-19 09:31:46 +0100

    Update wallpaper and dolphin settings

--- plasma5-defaults-zigzag/usr/share/plasma/look-and-feel/io.github.zigzag-linux.classic.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
+++ plasma5-defaults-zigzag/usr/share/plasma/look-and-feel/io.github.zigzag-linux.classic.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
@@ -2,7 +2,7 @@ applet.wallpaperPlugin = "org.kde.image";
 applet.currentConfigGroup = new Array("Wallpaper", "org.kde.image", "General");
 applet.writeConfig(
   "Image",
-  "file:///usr/share/wallpapers/openSUSEdefault/contents/images/1920x1080.jpg"
+  "file:///usr/share/wallpapers/openSUSEdefault/contents/images/5120x3200.jpg"
 );
 applet.writeConfig("FillMode", "2");
 applet.currentConfigGroup = new Array("General");
